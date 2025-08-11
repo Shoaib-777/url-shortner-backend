ff3973ad683f586a342fd3560f049209408a137d
Shoaib-777 <shaikmohammedshoaib777@gmail.com>
2025-08-12 00:13:50 +0530

    updated

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -58,13 +58,13 @@ export const UserLogin = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
         });
         res.cookie("user_id", user._id, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
         })
 
@@ -86,7 +86,7 @@ export const UserLogout = (req, res) => {
     res.clearCookie("token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict"
+        sameSite: "none"
     });
     return res.status(200).json({ message: "Logged out successfully" });
 };
